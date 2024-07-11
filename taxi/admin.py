dacff59e1ec1cfd2f8818a6aa9ed62a40d86f1e2
Ivan Vahanov <ivan.seeker6@gmail.com>
2024-07-11 16:03:51 +0300

    sol3

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,39 +1,34 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Manufacturer, Driver, Car
+from .models import Driver, Car, Manufacturer
 
 
-@admin.register(Manufacturer)
-class ManufacturerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'country')
-    search_fields = ('name', 'country')
-
-
+@admin.register(Driver)
 class DriverAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'license_number')
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
-        ('Additional info', {'fields': ('license_number',)}),
+    list_display = UserAdmin.list_display + ("license_number",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("license_number",)}),)
     )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'license_number'),
-        }),
-        ('Personal info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "license_number",
+                    )
+                },
+            ),
+        )
     )
-
-
-admin.site.register(Driver, DriverAdmin)
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('model', 'manufacturer')
-    search_fields = ('model',)
-    list_filter = ('manufacturer',)
+    search_fields = ("model",)
+    list_filter = ("manufacturer",)
+
+
+admin.site.register(Manufacturer)
